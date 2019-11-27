0d8684a5334fd7768e9fd8ad0314e67660effc58
swiatekmateusz <swiatek.mateusz.m@gmail.com>
2019-11-27 23:32:33 +0100

    Prepare for deploy

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongosee = require('mongoose')
+
+const UserSchema = mongosee.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+})
+
+module.exports = mongosee.model('user', UserSchema)
